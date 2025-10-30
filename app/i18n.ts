b352a6e34469dae0c51a9830754ec3cd05a4511d
Stoil100 <toli100ilov@gmail.com>
2025-10-30 23:24:44 +0200

    feat: Add localization support with i18next and Locize integration (Not ready)

---
+++ app/i18n.ts
@@ -0,0 +1,33 @@
+import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import LocizeBackend from "i18next-locize-backend";
+import { locizePlugin } from "locize";
+import LastUsed from "locize-lastused";
+import { initReactI18next } from "react-i18next";
+
+const isDev = import.meta.env.DEV;
+
+if(isDev) {
+  i18n.use(LastUsed);
+}
+
+const locizeOptions = {
+    projectId: import.meta.env.VITE_LOCIZE_PROJECT_ID,
+    apiKey: import.meta.env.VITE_LOCIZE_API_KEY,
+    version: import.meta.env.VITE_LOCIZE_VERSION,
+}
+
+i18n
+    .use(initReactI18next)
+    .use(LanguageDetector)
+    .use(LocizeBackend)
+    .use(locizePlugin)
+    .init({
+    debug: isDev,
+    fallbackLng: "bg",
+    backend: locizeOptions,
+    locizeLastUsed: locizeOptions,
+    saveMissing: isDev,
+});
+
+export default i18n;
