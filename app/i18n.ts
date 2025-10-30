d1614396dd6719fff75e19aaa95b9e983e000d16
Stoil100 <toli100ilov@gmail.com>
2025-10-30 23:42:35 +0200

    refactor: Clean up code formatting and improve readability in i18n and root components

--- app/i18n.ts
+++ app/i18n.ts
@@ -1,14 +1,14 @@
-import i18n from "i18next";
-import LanguageDetector from "i18next-browser-languagedetector";
-import LocizeBackend from "i18next-locize-backend";
-import { locizePlugin } from "locize";
-import LastUsed from "locize-lastused";
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next'
+import LanguageDetector from 'i18next-browser-languagedetector'
+import LocizeBackend from 'i18next-locize-backend'
+import { locizePlugin } from 'locize'
+import LastUsed from 'locize-lastused'
+import { initReactI18next } from 'react-i18next'
 
-const isDev = import.meta.env.DEV;
+const isDev = import.meta.env.DEV
 
-if(isDev) {
-  i18n.use(LastUsed);
+if (isDev) {
+    i18n.use(LastUsed)
 }
 
 const locizeOptions = {
@@ -17,17 +17,16 @@ const locizeOptions = {
     version: import.meta.env.VITE_LOCIZE_VERSION,
 }
 
-i18n
-    .use(initReactI18next)
+i18n.use(initReactI18next)
     .use(LanguageDetector)
     .use(LocizeBackend)
     .use(locizePlugin)
     .init({
-    debug: isDev,
-    fallbackLng: "bg",
-    backend: locizeOptions,
-    locizeLastUsed: locizeOptions,
-    saveMissing: isDev,
-});
+        debug: isDev,
+        fallbackLng: 'bg',
+        backend: locizeOptions,
+        locizeLastUsed: locizeOptions,
+        saveMissing: isDev,
+    })
 
-export default i18n;
+export default i18n
