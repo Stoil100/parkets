11575b5112817cdf08c0ed003fa42ebb41ae83e2
Stoil100 <toli100ilov@gmail.com>
2025-12-03 10:46:18 +0200

    feat: Add MovingTextSection component for animated marquee text

---
+++ app/components/pages/home/MovingText.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function MovingTextSection() {
+    return (
+        <div className="overflow-hidden whitespace-nowrap">
+            <h1 className="animate-marquee font-webserveroff text-golden uppercase text-8xl inline-block">
+                Royal Decorators | Прецизност, Стил, Съвършенство • Royal
+                Decorators | Прецизност, Стил, Съвършенство •
+            </h1>
+        </div>
+    )
+}
