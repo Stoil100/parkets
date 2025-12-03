f563b46ac4b34e24a0d2ab4ee36b4264429ec136
Stoil100 <toli100ilov@gmail.com>
2025-12-03 10:46:58 +0200

    feat: Update Home component to include SolutionsSection and adjust imports for better organization

--- app/root.tsx
+++ app/root.tsx
@@ -9,7 +9,7 @@ import {
 
 import type { Route } from './+types/root'
 import './app.css'
-import Navigation from './components/navigation/Navigation'
+import Navigation from './components/global/Navigation'
 import './i18n'
 
 export const links: Route.LinksFunction = () => [
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,10 @@ import HeroSection from '~/components/pages/home/Hero'
 import ServicesSection from '~/components/pages/home/Services'
 import VideoSection from '~/components/pages/home/Video'
 import type { Route } from './+types/home'
+import { ConsultationSection } from '~/components/pages/home/Consultation'
+import MovingTextSection from '~/components/pages/home/MovingText'
+import { BenefitsSection } from '~/components/pages/home/Benefits'
+import { SolutionsSection } from '~/components/pages/home/Solutions'
 
 export function meta({}: Route.MetaArgs) {
     return [
@@ -14,8 +18,12 @@ export default function Home() {
     return (
         <main>
             <HeroSection />
+            <ConsultationSection />
+            <MovingTextSection />
             <VideoSection />
+            <SolutionsSection />
             <ServicesSection />
+            <BenefitsSection />
         </main>
     )
 }
