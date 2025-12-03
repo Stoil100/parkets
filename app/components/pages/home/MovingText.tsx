a436a73552121aad3190f3019fd8696a0e8a149a
Stoil100 <toli100ilov@gmail.com>
2025-12-03 11:57:10 +0200

    feat: Implement marquee effect in MovingTextSection with updated styles

--- app/components/pages/home/MovingText.tsx
+++ app/components/pages/home/MovingText.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
+const text =
+    'Royal Decorators | Прецизност, Стил, Съвършенство • Royal Decorators | Прецизност, Стил, Съвършенство • '
+
 export default function MovingTextSection() {
     return (
-        <div className="overflow-hidden whitespace-nowrap">
-            <h1 className="animate-marquee font-webserveroff text-golden uppercase text-8xl inline-block">
-                Royal Decorators | Прецизност, Стил, Съвършенство • Royal
-                Decorators | Прецизност, Стил, Съвършенство •
-            </h1>
+        <div className="overflow-hidden py-10">
+            <div className="marquee-track">
+                <span className="marquee-item">{text}</span>
+                <div className="w-8" />
+                <span className="marquee-item" aria-hidden="true">
+                    {text}
+                </span>
+            </div>
         </div>
     )
 }
